5a334d586edceeb5a9bc6ca237479bd615011b1e
patoilp1 <patoilp1@niksula.hut.fi>
2015-01-14 01:39:33 +0200

    divided index.html to multiple templates with inheritance

--- wsd_games/games/views.py
+++ wsd_games/games/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.http import Http404
+from games.models import Game
 
 # Create your views here.
 def home(request):
-	return render_to_response('games/index.html')
+	try:
+		games = Game.objects.all()
+	except Games.DoesNotExist:
+		raise Http404
+	return render_to_response('games/base_grid_card.html', {'games': games})
 
 def games_list(request):
     raise Http404
