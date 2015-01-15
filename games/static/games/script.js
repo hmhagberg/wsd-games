a937ccf9a3cd5259c1e0b82eeb867cd0b1ad3fe1
patoilp1 <patoilp1@niksula.hut.fi>
2015-01-15 18:10:10 +0200

    removed pagination

--- games/static/games/script.js
+++ games/static/games/script.js
@@ -1,19 +1,3 @@
 $(document).ready(function() {
 
-	// Pagination
-	// Change active page number
-	$(".page-number").click(function() {
-		$(".active").removeClass("active");
-		$(this).addClass("active");
-	});
-
-	$(".pagination-prev").click(function() {
-		$(".active").removeClass("active");
-		$(this).addClass("active");
-	});
-
-	$(".pagination-next").click(function() {
-		$(".active").removeClass("active");
-		$(this).addClass("active");
-	});
 });
