1c29e68af9e43fa8c1f2c2b77e8ba996f82ddf0b
Muusssi <tommi.oinonen@gmail.com>
2015-01-15 16:13:33 +0200

    first commit to tests

--- wsd_games/games/tests.py
+++ wsd_games/games/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
+import unittest
 
-# Create your tests here.
+class TestBasicClient(unittest.TestCase):
+
+	def setUp(self):
+		self.client = Client()
+
+	def test_client_homepage(self):
+		response = self.client.get('/games/')
+		self.assertEqual(response.status_code, 200, "200 on '/'")
+		print('test works')
+
+if __name__ == '__main__':
+    unittest.main()
