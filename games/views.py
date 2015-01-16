fef319f2ea766e9caed4e65f8a81449c90527aeb
patoilp1 <patoilp1@niksula.hut.fi>
2015-01-16 20:17:41 +0200

    recognize if user is logged in, user data can be used in templates, changed the way that the views give data to templates and deleted duplicate views

--- games/views.py
+++ games/views.py
@@ -5,15 +5,18 @@ from django.http import Http404
 from games.models import *
 from django.contrib.auth import authenticate, login
 from games.forms import SignupForm
+from django.template import RequestContext
 
+context = {}
 
 def home(request):
     try:
         games = Game.objects.all()
         categories = Category.objects.all()
+        context.update({'games': games, 'categories': categories})
     except Game.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_grid_gameCard.html', {'games': games, 'categories': categories})
+    return render_to_response('games/base_grid_gameCard.html', context, context_instance = RequestContext(request))
 
 
 def signup(request):
@@ -40,66 +43,47 @@ def games_list(request):
 def categories_list(request):
     try:
         categories = Category.objects.all()
+        context.update({'categories': categories})
     except Category.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_grid_categoryCard.html', {'categories': categories})
+    return render_to_response('games/base_grid_categoryCard.html', context, context_instance = RequestContext(request))
 
 
 def developers_list(request):
     try:
         developers = Developer.objects.all()
         categories = Category.objects.all()
+        context.update({'developers': developers, 'categories': categories})
     except Developer.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_grid_developerCard.html',
-                              {'developers': developers, 'categories': categories})
+    return render_to_response('games/base_grid_developerCard.html', context, context_instance = RequestContext(request))
 
 
 def game(request, game_slug):
-	try:
-		game = Game.objects.get(slug = game_slug)
-		categories = Category.objects.all()
-	except Game.DoesNotExist:
-		raise Http404
-	return render_to_response('games/base_game.html', {'game': game, 'categories': categories})
-
-
-def category(request, category_slug):
-	try:
-		category = Category.objects.get(slug = category_slug)
-		games = Game.objects.filter(categories = category_slug)
-		categories = Category.objects.all()
-	except Category.DoesNotExist:
-		raise Http404
-	return render_to_response('games/base_grid_gameCard.html', {'category': category, 'games': games, 'categories': categories})
-
-
-def developer(request, developers_slug):
     try:
         game = Game.objects.get(slug = game_slug)
         categories = Category.objects.all()
+        context.update({'game': game, 'categories': categories})
     except Game.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_game.html', {'game': game, 'categories': categories})
-
+    return render_to_response('games/base_game.html', context, context_instance = RequestContext(request))
 
 def category(request, category_slug):
     try:
         category = Category.objects.get(slug = category_slug)
         games = category.category_games.all()
         categories = Category.objects.all()
+        context.update({'category': category, 'games': games, 'categories': categories, 'developer': ''})
     except Category.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_grid_gameCard.html',
-                              {'category': category, 'games': games, 'categories': categories})
-
+    return render_to_response('games/base_grid_gameCard.html', context, context_instance = RequestContext(request))
 
 def developer(request, developers_slug):
     try:
         developer = Developer.objects.get(slug = developers_slug)
         games = developer.developers_games.all()
         categories = Category.objects.all()
+        context.update({'developer': developer, 'games': games, 'categories': categories, 'category': ''})
     except Developer.DoesNotExist:
         raise Http404
-    return render_to_response('games/base_grid_gameCard.html',
-                              {'developer': developer, 'games': games, 'categories': categories})
+    return render_to_response('games/base_grid_gameCard.html', context, context_instance = RequestContext(request))
