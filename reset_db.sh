d0bee57715a0060190d79320b5e203a2bade5241
Muusssi <tommi.oinonen@gmail.com>
2015-01-25 20:37:52 +0200

    added script for resetting the database

---
+++ reset_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script for resetting the database
+
+# remove old database:
+rm db.sqlite3
+# remove old migrations:
+rm games/migrations/*
+# make and do migrations:
+python3 manage.py makemigrations
+python3 manage.py migrate
+# load fixture
+python3 manage.py loaddata games/fixtures/wsd-games-data.xml
