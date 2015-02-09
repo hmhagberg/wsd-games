6f1bf1647a1e176d0d3b38ab2717a7fd05d23943
hmhagberg <hmhagberg@gmail.com>
2015-02-09 09:28:42 +0200

    Updated migrations

--- games/migrations/0001_initial.py
+++ games/migrations/0001_initial.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.utils.timezone
-import django.core.validators
 from django.conf import settings
+import django.core.validators
 
 
 class Migration(migrations.Migration):
@@ -17,20 +17,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='WsdGamesUser',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
-                ('is_superuser', models.BooleanField(verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.', default=False)),
-                ('username', models.CharField(unique=True, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')], max_length=30, verbose_name='username', help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.')),
-                ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
-                ('last_name', models.CharField(blank=True, max_length=30, verbose_name='last name')),
-                ('email', models.EmailField(blank=True, max_length=75, verbose_name='email address')),
-                ('is_staff', models.BooleanField(verbose_name='staff status', help_text='Designates whether the user can log into this admin site.', default=False)),
-                ('is_active', models.BooleanField(verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', default=True)),
-                ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
+                ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
+                ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
+                ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('api_token', models.CharField(unique=True, max_length=40)),
-                ('groups', models.ManyToManyField(blank=True, related_query_name='user', help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', to='auth.Group', verbose_name='groups', related_name='user_set')),
-                ('user_permissions', models.ManyToManyField(blank=True, related_query_name='user', help_text='Specific permissions for this user.', to='auth.Permission', verbose_name='user permissions', related_name='user_set')),
+                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
@@ -42,11 +42,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255)),
                 ('slug', models.SlugField()),
-                ('image_url', models.URLField(blank=True, default='http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif')),
-                ('description', models.TextField(default='Category description')),
+                ('image_url', models.URLField(default=b'http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif', blank=True)),
+                ('description', models.TextField(default=b'Category description')),
             ],
             options={
                 'ordering': ['name'],
@@ -57,11 +57,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Developer',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255)),
                 ('slug', models.SlugField()),
-                ('image_url', models.URLField(blank=True, default='http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif')),
-                ('description', models.TextField(default='Developer description')),
+                ('image_url', models.URLField(default=b'http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif', blank=True)),
+                ('description', models.TextField(default=b'Developer description')),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
@@ -73,15 +73,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Game',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(unique=True, max_length=255)),
                 ('slug', models.SlugField()),
-                ('description', models.TextField(default='Game description')),
+                ('description', models.TextField(default=b'Game description')),
                 ('url', models.URLField()),
-                ('image_url', models.URLField(blank=True, default='http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif')),
-                ('price', models.DecimalField(validators=[django.core.validators.MinValueValidator(0)], decimal_places=2, max_digits=6)),
+                ('image_url', models.URLField(default=b'http://rammb.cira.colostate.edu/dev/hillger/WSD_logo.gif', blank=True)),
+                ('price', models.DecimalField(max_digits=6, decimal_places=2, validators=[django.core.validators.MinValueValidator(0)])),
                 ('categories', models.ManyToManyField(related_name='games', to='games.Category')),
-                ('developer', models.ForeignKey(to='games.Developer', related_name='games')),
+                ('developer', models.ForeignKey(related_name='games', to='games.Developer')),
             ],
             options={
                 'ordering': ['name'],
@@ -92,12 +92,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Ownership',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('highscore', models.PositiveIntegerField(default=0)),
                 ('saved_score', models.PositiveIntegerField(default=0)),
-                ('saved_data', models.TextField(default='[]')),
-                ('rating', models.PositiveIntegerField(choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)], null=True)),
-                ('game', models.ForeignKey(to='games.Game', related_name='ownerships')),
+                ('saved_data', models.TextField(default=b'[]')),
+                ('rating', models.PositiveIntegerField(null=True, choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)])),
+                ('game', models.ForeignKey(related_name='ownerships', to='games.Game')),
             ],
             options={
                 'ordering': ['-highscore'],
@@ -107,12 +107,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Payment',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('completed', models.BooleanField(default=False)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('pid', models.CharField(unique=True, max_length=32)),
-                ('ref', models.CharField(blank=True, max_length=32)),
-                ('game', models.ForeignKey(to='games.Game', related_name='sales')),
+                ('ref', models.CharField(max_length=32, blank=True)),
+                ('game', models.ForeignKey(related_name='sales', to='games.Game')),
             ],
             options={
             },
@@ -121,8 +121,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Player',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('slug', models.SlugField()),
+                ('about_me', models.TextField(blank=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
@@ -132,7 +133,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SignupActivation',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('key', models.CharField(unique=True, max_length=32)),
                 ('time_sent', models.DateTimeField(auto_now_add=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
@@ -144,13 +145,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='payment',
             name='player',
-            field=models.ForeignKey(to='games.Player', related_name='payments'),
+            field=models.ForeignKey(related_name='payments', to='games.Player'),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='ownership',
             name='player',
-            field=models.ForeignKey(to='games.Player', related_name='ownerships'),
+            field=models.ForeignKey(related_name='ownerships', to='games.Player'),
             preserve_default=True,
         ),
     ]
