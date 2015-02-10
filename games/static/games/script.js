0b9c7ee4886c6e98d2ff4bf3812e5bba23ce3f25
patoilp1 <patoilp1@niksula.hut.fi>
2015-02-10 15:50:04 +0200

    fixed search a little cleaned and commented base_grid.html, test mobile scaling

--- games/static/games/script.js
+++ games/static/games/script.js
@@ -8,4 +8,18 @@ $(document).ready(function() {
     window.setTimeout(function(){
         $('.form-field').tooltip('hide');
     }, 3500);
+
+    // Search (base_grid.html)
+    // http://stackoverflow.com/questions/8746882/jquery-contains-selector-uppercase-and-lower-case-issue
+    jQuery.expr[':'].contains = function(a, i, m) {
+        return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    };
+    $("#searchForm").submit(function(event) {
+        var search = $("#search").val();
+        $(".col-xs-12").each(function(){
+           $(this).removeClass("hidden");
+           $(this).not(':contains(' + search + ')').addClass("hidden");
+        });
+        event.preventDefault();
+    });
 });
